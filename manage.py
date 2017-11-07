2194d05f44e8c3289bbc0876ed63ce1bb3e5fd13
Isaac <iybaron25@gmail.com>
2017-11-06 20:08:34 -0800

    Added basic unit tests

--- manage.py
+++ manage.py
@@ -19,5 +19,12 @@ manager.add_command('shell', Shell(make_context=make_shell_context))
 # Provides easy command line database migration
 manager.add_command('db', MigrateCommand)
 
+# 
+@manager.command
+def test():
+	import unittest
+	tests = unittest.TestLoader().discover('tests')
+	unittest.TextTestRunner(verbosity=2).run(tests)
+
 if __name__ == "__main__":
 	manager.run()
